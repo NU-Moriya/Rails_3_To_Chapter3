3e7896b27e9a7faa3ca9160d097fd2464ef32bb6
nobuko.m <animopara100pre@gmail.com>
2020-08-13 11:35:43 +0000

    To_Chapter3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'aac5b9a3c47b015ac3c42aa0b746d413c649cbcc2c4f83c1f026f27d2d54810f7423e3d0939d25020c071223b2e21e2d9d20d6392aef9be89f5a54365462b10c'
